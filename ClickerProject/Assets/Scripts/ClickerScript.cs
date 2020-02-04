ad9a97e9cc2f7e96a86878263fa7aa1a0ef1dc14
Kevincouper <fawderfox@gmail.com>
2020-02-04 03:28:06 +0100

    Clicking gives score, score displayed. Click should work on mobile

---
+++ ClickerProject/Assets/Scripts/ClickerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickerScript : MonoBehaviour
+{
+    private int score = 0;
+
+
+    
+
+    public void AddScore()
+    {
+        score += 1;
+    }
+    
+    public int GetScore()
+    {
+        return score;
+    }
+}
---
+++ ClickerProject/Assets/Scripts/ScoreDisplayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreDisplayer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreDisplay;
+    [SerializeField] ClickerScript clickerScript;
+
+
+    void Update()
+    {
+        String textScore = clickerScript.GetScore().ToString();
+        scoreDisplay.text = textScore;
+    }
+}
